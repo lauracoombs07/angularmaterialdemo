455b21d2d708d4c0108b563306f4eecd1049b922
Laura <laura.coombs07@gmail.com>
2018-11-21 05:56:33 -0700

    radiobutton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  isChecked = true;
+
+  onChange($event) {
+    console.log($event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { 
+  MatCheckboxModule,
+  MatRadioModule 
+} from '@angular/material';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -14,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
